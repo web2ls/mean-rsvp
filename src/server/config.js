056f1ebc8141bfda76673bfdea651fd091fefab0
web2ls <web2ls@yandex.ru>
2018-07-16 22:57:10 +0300

    basic setup for MongoDB & Nodejs

---
+++ src/server/config.js
@@ -0,0 +1,6 @@
+// server/config.js
+module.exports = {
+  AUTH0_DOMAIN: 'web2ls.eu.auth0.com', // e.g., kmaida.auth0.com
+  AUTH0_API_AUDIENCE: 'https://web2ls.eu.auth0.com/api/v2/', // e.g., 'http://localhost:8083/api/'
+  MONGO_URI: 'mongodb://web2ls:suslan-007@ds239681.mlab.com:39681/mean'
+};
