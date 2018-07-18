cf9497b0cea9f2036c6c41daa309e2436513551d
web2ls <web2ls@yandex.ru>
2018-07-18 19:44:45 +0300

    Added Layout and Header components

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const config = require('./server/config');
  |--------------------------------------
  */
 
-mongoose.connect(config.MONGO_URI);
+mongoose.connect(config.MONGO_URI, { useNewUrlParser: true });
 const monDb = mongoose.connection;
 
 monDb.on('error', function() {
